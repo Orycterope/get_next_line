276c02c531142d7bbb9facf406cbc0ef587da2de
Thomas VERMEILH <tvermeil@e2r3p3.42.fr>
2015-12-12 20:30:49 +0100

    norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,13 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 14:49:57 by tvermeil          #+#    #+#             */
-/*   Updated: 2015/12/12 20:13:32 by tvermeil         ###   ########.fr       */
+/*   Updated: 2015/12/12 20:29:41 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 static t_pendant	*pendant;
-
-/*
-** fonction qui coupe une chaine en deux sur le \n
-** et stock la deuxieme partie dans pendant
-** retourne le premier bout de chaine
-*/
-
-/*
-** free pendant et renvoie une chaine malloc !
-** ne free pas la str car peut etre le buffer.
-*/
 
 static t_pendant	*get_pendant_fd(int fd)
 {
@@ -87,10 +76,10 @@ static void			remove_pendant_fd(int fd)
 	}
 }
 
-static char	*split_str(char *str, int fd)
+static char			*split_str(char *str, int fd)
 {
 	int			i;
-	t_pendant	*previous_pendant; /* copie du pointeur pour le free */ // not needed anymore;
+	t_pendant	*previous_pendant;
 
 	previous_pendant = get_pendant_fd(fd);
 	i = 0;
@@ -100,7 +89,7 @@ static char	*split_str(char *str, int fd)
 		{
 			add_pendant_fd(fd, ft_strdup(&str[i + 1]));
 			str[i] = '\0';
-			break;
+			break ;
 		}
 		i++;
 	}
@@ -109,11 +98,11 @@ static char	*split_str(char *str, int fd)
 	return (str);
 }
 
-int		get_next_line(int const fd, char **line)
+int					get_next_line(int const fd, char **line)
 {
 	char		buffer[BUFF_SIZE + 1];
 	char		*join;
-	char		*cpy; /* ??? */;
+	char		*cpy;
 	int			read_nbr;
 	t_pendant	*pendant_fd;
 
@@ -122,24 +111,16 @@ int		get_next_line(int const fd, char **line)
 	join = NULL;
 	pendant_fd = get_pendant_fd(fd);
 	if (pendant_fd != NULL)
-	{
-		join = split_str(ft_strdup(pendant_fd->str), fd); // No free required;
-	}
-	read_nbr = 1; // cas ou rien n'est lu car pendant n'es pas vide // SUR ?;
-	while ((pendant_fd = get_pendant_fd(fd)) == NULL && (read_nbr = read(fd, buffer, BUFF_SIZE)) != 0)
+		join = split_str(ft_strdup(pendant_fd->str), fd);
+	read_nbr = 1;
+	while ((pendant_fd = get_pendant_fd(fd)) == NULL
+			&& (read_nbr = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[read_nbr] = '\0';
 		cpy = join;
-		if (read_nbr == -1)
-		{
-			free(join);
-			//free(pendant_fd->str);
-			//free(pendant_fd);	/* si free pendant la fonction ne peut pas etre rappellee apres une erreur */;
-			return (-1);	/* peut gagner une ligne en mettant ce block a la fin et faire un seul return */;
-		}
 		join = ft_strjoin(join, split_str(buffer, fd));
 		free(cpy);
 	}
 	*line = join;
-	return ((read_nbr > 0) ? 1 : 0);
+	return ((read_nbr > 0) ? 1 : read_nbr);
 }
